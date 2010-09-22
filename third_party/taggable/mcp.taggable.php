49e9f795d09d04b9c7d958bfe53d560b48e276ba
Jamie Rumbelow <jamie@jamierumbelow.net>
2010-09-22 18:29:27 +0100

    Fixed a quick CP bug

--- third_party/taggable/mcp.taggable.php
+++ third_party/taggable/mcp.taggable.php
@@ -605,13 +605,13 @@ class Taggable_mcp {
 	 * @author Jamie Rumbelow
 	 */
 	private function _theme_url() {
-		if (!isset($this->EE->session->cache['taggable']['theme_url'])) {
-			$theme_folder_url = $this->EE->config->item('theme_folder_url');
+		if (!isset($this->ee->session->cache['taggable']['theme_url'])) {
+			$theme_folder_url = $this->ee->config->item('theme_folder_url');
 			if (substr($theme_folder_url, -1) != '/') $theme_folder_url .= '/';
-			$this->EE->session->cache['taggable']['theme_url'] = $theme_folder_url.'third_party/taggable/';
+			$this->ee->session->cache['taggable']['theme_url'] = $theme_folder_url.'third_party/taggable/';
 		}
 
-		return $this->EE->session->cache['taggable']['theme_url'];
+		return $this->ee->session->cache['taggable']['theme_url'];
 	}
 	
 	// ------
@@ -636,7 +636,7 @@ class Taggable_mcp {
 	 * @author Jamie Rumbelow
 	 */
 	private function _get_themes() {
-		if (!isset($this->EE->session->cache['taggable']['themes'])) {
+		if (!isset($this->ee->session->cache['taggable']['themes'])) {
 			$theme_folder_path = $this->ee->config->item('theme_folder_path');
 			if (substr($theme_folder_path, -1) != '/') $theme_folder_path .= '/';
 			
@@ -649,9 +649,9 @@ class Taggable_mcp {
 				}
 			}
 			
-			$this->EE->session->cache['taggable']['themes'] = $themes;
+			$this->ee->session->cache['taggable']['themes'] = $themes;
 		}
 		
-		return $this->EE->session->cache['taggable']['themes'];
+		return $this->ee->session->cache['taggable']['themes'];
 	}
 }
