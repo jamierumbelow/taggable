cf2bcb30d1c93a80350317c64be49e7b3dd180fa
Jamie Rumbelow <jamie@jamierumbelow.net>
2011-02-26 18:46:01 +0000

    Adding the documentation

---
+++ docs/pages/sort.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Taggable
+ *
+ * A powerful, easy to use folksonomy
+ * engine for ExpressionEngine 2.0.
+ *
+ * @author Jamie Rumbelow <http://jamierumbelow.net>
+ * @copyright Copyright (c)2010 Jamie Rumbelow
+ * @license http://getsparkplugs.com/taggable/docs#license
+ * @version 1.4.1
+ **/
+
+function sparkplugs_doc_sort($a, $b) {
+	$order = array(
+		'introduction.html', 'fieldtype.html', 'module.html', 'developers.html'
+	);
+	
+	return (array_search($a, $order) > array_search($b, $order)) ? 1 : -1;
+}
