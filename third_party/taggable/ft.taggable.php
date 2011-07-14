607e924e52691daa3be973c873963f5458b37214
Jamie Rumbelow <jamie@jamierumbelow.net>
2011-07-14 15:09:51 +0100

    Bumped to version 1.4.5

--- third_party/taggable/ft.taggable.php
+++ third_party/taggable/ft.taggable.php
@@ -8,7 +8,7 @@
  * @author Jamie Rumbelow <http://jamierumbelow.net>
  * @copyright Copyright (c)2010 Jamie Rumbelow
  * @license http://getsparkplugs.com/taggable/docs#license
- * @version 1.4.2
+ * @version 1.4.5
  **/
 
 require_once PATH_THIRD."taggable/libraries/Model.php";
